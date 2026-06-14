513ab5139d87425bda5d144b9366f3514ca63a25
Reagan Musa <reagan.musav@gmail.com>
2026-06-14 21:27:44 +0300

    favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -83,6 +83,11 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: "/favicon.svg",
+      },
+      {
         rel: "preconnect",
         href: "https://fonts.googleapis.com",
       },
